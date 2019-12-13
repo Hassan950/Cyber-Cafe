f6b79a008d6bc2d7305070f703793d204e6b3344
Darkin <darkinb255@gmail.com>
2019-12-13 20:52:48 +0200

    WinForm

---
+++ CCafe/WindowsFormsApp3/CMain.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class CMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GameCB = new System.Windows.Forms.ComboBox();
+            this.ReserveBTN = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.Date = new System.Windows.Forms.DateTimePicker();
+            this.StartTime = new System.Windows.Forms.DateTimePicker();
+            this.EndTime = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SubBTN = new System.Windows.Forms.Button();
+            this.FeedBTN = new System.Windows.Forms.Button();
+            this.UserNameLp = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GameCB
+            // 
+            this.GameCB.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.GameCB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GameCB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.GameCB.FormattingEnabled = true;
+            this.GameCB.Location = new System.Drawing.Point(406, 467);
+            this.GameCB.Name = "GameCB";
+            this.GameCB.Size = new System.Drawing.Size(400, 25);
+            this.GameCB.TabIndex = 0;
+            this.GameCB.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // ReserveBTN
+            // 
+            this.ReserveBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.ReserveBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ReserveBTN.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.ReserveBTN.FlatAppearance.BorderSize = 0;
+            this.ReserveBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ReserveBTN.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReserveBTN.ForeColor = System.Drawing.Color.White;
+            this.ReserveBTN.Location = new System.Drawing.Point(876, 603);
+            this.ReserveBTN.Name = "ReserveBTN";
+            this.ReserveBTN.Size = new System.Drawing.Size(369, 84);
+            this.ReserveBTN.TabIndex = 7;
+            this.ReserveBTN.Text = "RESERVE";
+            this.ReserveBTN.UseVisualStyleBackColor = false;
+            this.ReserveBTN.Click += new System.EventHandler(this.ReserveBTN_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(406, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(845, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(845, 467);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(400, 25);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // Date
+            // 
+            this.Date.CalendarForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.Date.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Date.Location = new System.Drawing.Point(406, 541);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(400, 32);
+            this.Date.TabIndex = 13;
+            // 
+            // StartTime
+            // 
+            this.StartTime.CalendarForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.StartTime.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.StartTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.StartTime.Location = new System.Drawing.Point(845, 541);
+            this.StartTime.Name = "StartTime";
+            this.StartTime.Size = new System.Drawing.Size(191, 32);
+            this.StartTime.TabIndex = 14;
+            // 
+            // EndTime
+            // 
+            this.EndTime.CalendarForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.EndTime.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.EndTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.EndTime.Location = new System.Drawing.Point(1054, 541);
+            this.EndTime.Name = "EndTime";
+            this.EndTime.Size = new System.Drawing.Size(191, 32);
+            this.EndTime.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.label3.Location = new System.Drawing.Point(401, 509);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 29);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "DATE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.label4.Location = new System.Drawing.Point(840, 509);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 29);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "START TIME";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.label5.Location = new System.Drawing.Point(1049, 509);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 29);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "END TIME";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.label1.Location = new System.Drawing.Point(840, 429);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 29);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "ROOM/CONSOLE";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.label6.Location = new System.Drawing.Point(401, 429);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 29);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "GAME";
+            // 
+            // SubBTN
+            // 
+            this.SubBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.SubBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.SubBTN.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.SubBTN.FlatAppearance.BorderSize = 0;
+            this.SubBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SubBTN.Font = new System.Drawing.Font("Tahoma", 18F);
+            this.SubBTN.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.SubBTN.Location = new System.Drawing.Point(0, 406);
+            this.SubBTN.Margin = new System.Windows.Forms.Padding(0);
+            this.SubBTN.Name = "SubBTN";
+            this.SubBTN.Size = new System.Drawing.Size(382, 84);
+            this.SubBTN.TabIndex = 4;
+            this.SubBTN.Text = "SUBSCRIPTIONS";
+            this.SubBTN.UseVisualStyleBackColor = false;
+            this.SubBTN.Click += new System.EventHandler(this.SubBTN_Click);
+            // 
+            // FeedBTN
+            // 
+            this.FeedBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.FeedBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.FeedBTN.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.FeedBTN.FlatAppearance.BorderSize = 0;
+            this.FeedBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.FeedBTN.Font = new System.Drawing.Font("Tahoma", 18F);
+            this.FeedBTN.ForeColor = System.Drawing.Color.White;
+            this.FeedBTN.Location = new System.Drawing.Point(0, 490);
+            this.FeedBTN.Margin = new System.Windows.Forms.Padding(0);
+            this.FeedBTN.Name = "FeedBTN";
+            this.FeedBTN.Size = new System.Drawing.Size(382, 84);
+            this.FeedBTN.TabIndex = 5;
+            this.FeedBTN.Text = "FEEDBACK";
+            this.FeedBTN.UseVisualStyleBackColor = false;
+            this.FeedBTN.Click += new System.EventHandler(this.FeedBTN_Click);
+            // 
+            // UserNameLp
+            // 
+            this.UserNameLp.AutoSize = true;
+            this.UserNameLp.Font = new System.Drawing.Font("Tahoma", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNameLp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(135)))), ((int)(((byte)(200)))));
+            this.UserNameLp.Location = new System.Drawing.Point(68, 244);
+            this.UserNameLp.Name = "UserNameLp";
+            this.UserNameLp.Size = new System.Drawing.Size(240, 46);
+            this.UserNameLp.TabIndex = 6;
+            this.UserNameLp.Text = "CUSTOMER#";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(26, 16);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(336, 205);
+            this.pictureBox3.TabIndex = 20;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.UserNameLp);
+            this.panel1.Controls.Add(this.FeedBTN);
+            this.panel1.Controls.Add(this.SubBTN);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(382, 720);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(0, 322);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(382, 84);
+            this.panel2.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Tahoma", 18F);
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(382, 84);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "HOME";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EndTime);
+            this.Controls.Add(this.StartTime);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ReserveBTN);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.GameCB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox GameCB;
+        private System.Windows.Forms.Button ReserveBTN;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DateTimePicker Date;
+        private System.Windows.Forms.DateTimePicker StartTime;
+        private System.Windows.Forms.DateTimePicker EndTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button SubBTN;
+        private System.Windows.Forms.Button FeedBTN;
+        private System.Windows.Forms.Label UserNameLp;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
