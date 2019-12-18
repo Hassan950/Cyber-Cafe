1bf5a51ef54ae816107855402b513186cdb75aad
Happysword <45149700+Happysword@users.noreply.github.com>
2019-12-19 00:37:49 +0200

    Finished Tournaments/Offers

--- CCafe/WindowsFormsApp3/MOT.Designer.cs
+++ CCafe/WindowsFormsApp3/MOT.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.IDLabel = new System.Windows.Forms.Label();
             this.UserNameLp = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
@@ -41,14 +43,30 @@
             this.OfferSelect = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.DataGrid = new System.Windows.Forms.DataGridView();
+            this.TournNameCB = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.TournNameTB = new System.Windows.Forms.TextBox();
+            this.StatusNewTB = new System.Windows.Forms.TextBox();
+            this.ParticNumNew = new System.Windows.Forms.NumericUpDown();
+            this.StartDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.AddTourn = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.EditTourn = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ParticNumNew)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.IDLabel);
             this.panel1.Controls.Add(this.UserNameLp);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.EditCyberBTN);
@@ -60,12 +78,23 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(382, 720);
             this.panel1.TabIndex = 22;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // IDLabel
+            // 
+            this.IDLabel.Font = new System.Drawing.Font("Century Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(197)))), ((int)(((byte)(80)))));
+            this.IDLabel.Location = new System.Drawing.Point(1, 215);
+            this.IDLabel.Name = "IDLabel";
+            this.IDLabel.Size = new System.Drawing.Size(382, 45);
+            this.IDLabel.TabIndex = 30;
+            this.IDLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // UserNameLp
             // 
             this.UserNameLp.Font = new System.Drawing.Font("Century Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.UserNameLp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(197)))), ((int)(((byte)(80)))));
-            this.UserNameLp.Location = new System.Drawing.Point(0, 200);
+            this.UserNameLp.Location = new System.Drawing.Point(3, 170);
             this.UserNameLp.Name = "UserNameLp";
             this.UserNameLp.Size = new System.Drawing.Size(382, 45);
             this.UserNameLp.TabIndex = 27;
@@ -189,13 +218,13 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(197)))), ((int)(((byte)(80)))));
-            this.label1.Location = new System.Drawing.Point(514, 384);
+            this.label1.Location = new System.Drawing.Point(475, 336);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 30);
+            this.label1.Size = new System.Drawing.Size(178, 23);
             this.label1.TabIndex = 33;
-            this.label1.Text = "ADD";
+            this.label1.Text = "New Tournament";
             // 
             // OfferSelect
             // 
@@ -217,25 +246,183 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(197)))), ((int)(((byte)(80)))));
-            this.label3.Location = new System.Drawing.Point(1032, 384);
+            this.label3.Location = new System.Drawing.Point(1000, 336);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 30);
+            this.label3.Size = new System.Drawing.Size(167, 23);
             this.label3.TabIndex = 38;
-            this.label3.Text = "EDIT";
+            this.label3.Text = "Edit Tournament";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // DataGrid
             // 
-            this.DataGrid.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DataGrid.BackgroundColor = System.Drawing.Color.Gray;
+            this.DataGrid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.DataGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.DataGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Chocolate;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Tahoma", 8F);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.DarkOrange;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.DataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DataGrid.Location = new System.Drawing.Point(421, 94);
+            this.DataGrid.Location = new System.Drawing.Point(435, 94);
             this.DataGrid.Name = "DataGrid";
             this.DataGrid.RowHeadersWidth = 51;
             this.DataGrid.RowTemplate.Height = 26;
-            this.DataGrid.Size = new System.Drawing.Size(811, 257);
+            this.DataGrid.Size = new System.Drawing.Size(811, 221);
             this.DataGrid.TabIndex = 37;
+            // 
+            // TournNameCB
+            // 
+            this.TournNameCB.Font = new System.Drawing.Font("Trajan Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TournNameCB.FormattingEnabled = true;
+            this.TournNameCB.Location = new System.Drawing.Point(1005, 389);
+            this.TournNameCB.Name = "TournNameCB";
+            this.TournNameCB.Size = new System.Drawing.Size(171, 29);
+            this.TournNameCB.TabIndex = 31;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(197)))), ((int)(((byte)(80)))));
+            this.label4.Location = new System.Drawing.Point(421, 392);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 21);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(197)))), ((int)(((byte)(80)))));
+            this.label5.Location = new System.Drawing.Point(665, 615);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 21);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Start Date";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(197)))), ((int)(((byte)(80)))));
+            this.label6.Location = new System.Drawing.Point(656, 562);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 21);
+            this.label6.TabIndex = 42;
+            this.label6.Text = "Particpants";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(197)))), ((int)(((byte)(80)))));
+            this.label7.Location = new System.Drawing.Point(938, 392);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 21);
+            this.label7.TabIndex = 46;
+            this.label7.Text = "Name";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(197)))), ((int)(((byte)(80)))));
+            this.label10.Location = new System.Drawing.Point(673, 503);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 21);
+            this.label10.TabIndex = 47;
+            this.label10.Text = "Status";
+            // 
+            // TournNameTB
+            // 
+            this.TournNameTB.Font = new System.Drawing.Font("Trajan Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TournNameTB.Location = new System.Drawing.Point(498, 389);
+            this.TournNameTB.Name = "TournNameTB";
+            this.TournNameTB.Size = new System.Drawing.Size(171, 29);
+            this.TournNameTB.TabIndex = 50;
+            // 
+            // StatusNewTB
+            // 
+            this.StatusNewTB.Font = new System.Drawing.Font("Trajan Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusNewTB.Location = new System.Drawing.Point(785, 503);
+            this.StatusNewTB.Name = "StatusNewTB";
+            this.StatusNewTB.Size = new System.Drawing.Size(171, 29);
+            this.StatusNewTB.TabIndex = 51;
+            this.StatusNewTB.Text = "ongoing";
+            // 
+            // ParticNumNew
+            // 
+            this.ParticNumNew.Font = new System.Drawing.Font("Trajan Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ParticNumNew.Location = new System.Drawing.Point(785, 562);
+            this.ParticNumNew.Name = "ParticNumNew";
+            this.ParticNumNew.Size = new System.Drawing.Size(171, 29);
+            this.ParticNumNew.TabIndex = 52;
+            // 
+            // StartDatePicker
+            // 
+            this.StartDatePicker.Font = new System.Drawing.Font("Trajan Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartDatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.StartDatePicker.Location = new System.Drawing.Point(785, 615);
+            this.StartDatePicker.Name = "StartDatePicker";
+            this.StartDatePicker.Size = new System.Drawing.Size(171, 29);
+            this.StartDatePicker.TabIndex = 54;
+            this.StartDatePicker.ValueChanged += new System.EventHandler(this.StartDatePicker_ValueChanged);
+            // 
+            // AddTourn
+            // 
+            this.AddTourn.BackColor = System.Drawing.Color.Gold;
+            this.AddTourn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddTourn.ForeColor = System.Drawing.Color.White;
+            this.AddTourn.Location = new System.Drawing.Point(515, 452);
+            this.AddTourn.Name = "AddTourn";
+            this.AddTourn.Size = new System.Drawing.Size(100, 50);
+            this.AddTourn.TabIndex = 55;
+            this.AddTourn.Text = "ADD";
+            this.AddTourn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.AddTourn.Click += new System.EventHandler(this.AddTourn_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(197)))), ((int)(((byte)(80)))));
+            this.label11.Location = new System.Drawing.Point(673, 679);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(89, 21);
+            this.label11.TabIndex = 57;
+            this.label11.Text = "End Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Trajan Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(785, 679);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(171, 29);
+            this.dateTimePicker1.TabIndex = 58;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // EditTourn
+            // 
+            this.EditTourn.BackColor = System.Drawing.Color.Gold;
+            this.EditTourn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditTourn.ForeColor = System.Drawing.Color.White;
+            this.EditTourn.Location = new System.Drawing.Point(1034, 452);
+            this.EditTourn.Name = "EditTourn";
+            this.EditTourn.Size = new System.Drawing.Size(100, 50);
+            this.EditTourn.TabIndex = 56;
+            this.EditTourn.Text = "EDIT";
+            this.EditTourn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.EditTourn.Click += new System.EventHandler(this.label11_Click);
             // 
             // MOT
             // 
@@ -243,6 +430,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.EditTourn);
+            this.Controls.Add(this.AddTourn);
+            this.Controls.Add(this.StartDatePicker);
+            this.Controls.Add(this.ParticNumNew);
+            this.Controls.Add(this.StatusNewTB);
+            this.Controls.Add(this.TournNameTB);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TournNameCB);
             this.Controls.Add(this.OfferSelect);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.DataGrid);
@@ -254,9 +455,11 @@
             this.Name = "MOT";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MOT";
+            this.Load += new System.EventHandler(this.MOT_Load);
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ParticNumNew)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -277,5 +480,20 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DataGridView DataGrid;
         private System.Windows.Forms.Label UserNameLp;
+        private System.Windows.Forms.Label IDLabel;
+        private System.Windows.Forms.ComboBox TournNameCB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox TournNameTB;
+        private System.Windows.Forms.TextBox StatusNewTB;
+        private System.Windows.Forms.NumericUpDown ParticNumNew;
+        private System.Windows.Forms.DateTimePicker StartDatePicker;
+        private System.Windows.Forms.Label AddTourn;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label EditTourn;
     }
 }
